76622f7ec07a054da4746ad2042edf941f93209a
CongTao <Congtao1997@gmail.com>
2018-12-07 11:11:42 +0700

    add

--- project/#/level_three/004/1.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-	$('.pics').isotope({
-		itemSelector:'li'
-	});
-});
--- project/#/level_two/005/1.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-
-	TweenMax.staggerFrom($('.part'),1,{opacity: 0,left: 300},0.3);
-	
-});
